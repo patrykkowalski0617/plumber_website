8ff10f6f4d749ffc3b2ed4d2b5e5bfb7f8541119
Patryk <patrykkowalski0617@gmail.com>
2019-02-04 21:40:45 +0100

    index: parallax

--- js/index/index.js
+++ js/index/index.js
@@ -1,6 +1,18 @@
 (function() {
-	window.addEventListener('scroll', function(){
-		console.log(window.pageYOffset)
-	})
-	
+	// Parallax effect
+	const parallax = {
+		scrollPosition: function(){
+			return window.pageYOffset;
+		},
+		transform: function(speed){
+			return this.scrollPosition() / speed;
+		},
+		transformCss: function(speed){return 'translateY(' + this.transform(speed) + 'px)'},
+		create: function(speed, element){
+			window.addEventListener('scroll', function(){
+				element.style.transform = parallax.transformCss(speed);
+			})
+		}
+	};
+	parallax.create(2, document.querySelector('#welcome-section'));
 })()
