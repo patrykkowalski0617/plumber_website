8af320a256eb662fb6921fc275a952f5f43d42e1
Patryk <patrykkowalski0617@gmail.com>
2019-04-25 07:55:16 +0200

    location marker - links level one

--- js/location_marker.js
+++ js/location_marker.js
@@ -2,7 +2,21 @@ window.addEventListener('load', function(){
 	'use strict'
 	
 	const locationMarker = function(){
-		console.log('location marker')
+		const location = window.location;
+		const fileName = function(el){
+			const href = el.href;
+			const index = href.lastIndexOf('/') + 1;
+			return href.substr(index);
+		};
+		const searchedFileName = fileName(location);
+		const linksLevel1 = document.querySelectorAll('#header nav ol > li > a');
+		const linksLevel2 = document.querySelectorAll('#header nav ul > li > a');
+
+		for (var i = 0; i < linksLevel1.length; i++) {
+			if (fileName(linksLevel1[i]) == searchedFileName) {
+				linksLevel1[i].classList.add('active')
+			}
+		}
 	};
 
 	locationMarker()
