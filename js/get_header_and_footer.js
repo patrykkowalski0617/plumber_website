1d953c62d1c28167588776392fdfc39dd1d727a7
Patryk <38049103+patrykkowalski0617@users.noreply.github.com>
2019-05-06 16:39:09 +0200

    get_header_and_footer for footer

--- js/get_header_and_footer.js
+++ js/get_header_and_footer.js
@@ -1,16 +1,17 @@
 window.addEventListener('load', function(){
 	'use strict'
 
-	function loadXMLDoc() {
+	function loadXMLDoc(targetElementId, fileName) {
 		var xhr = new XMLHttpRequest();
 		xhr.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
-				document.getElementById("header").innerHTML = this.responseText;
+				document.getElementById(targetElementId).innerHTML = this.responseText;
 			};
 		};
-		xhr.open("GET", "header.html", true);
+		xhr.open('GET', fileName, true);
 		xhr.send();
 	}
 
-	loadXMLDoc()
+	loadXMLDoc('header', 'header.html')
+	loadXMLDoc('footer', 'footer.html')
 })
